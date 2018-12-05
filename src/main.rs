e665fdef87c53a409ccb6a945d3c119c8ca3146f
Joel Pedraza <jpedraza@chewy.com>
2018-12-05 13:31:02 -0500

    problem 4 skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod problem01;
 mod problem02;
 mod problem03;
+mod problem04;
 mod problem05;
 
 fn main() {
@@ -12,6 +13,9 @@ fn main() {
 
     problem03::part1();
     problem03::part2();
+
+    problem04::part1();
+    problem04::part2();
 
     problem05::part1();
     problem05::part2();
---
+++ src/problem04/solution.rs
@@ -0,0 +1,45 @@
+use super::input::INPUT;
+
+pub fn part1() {
+    println!("Problem 4, Part 1: {}", "???")
+}
+
+pub fn part2() {
+    println!("Problem 4, Part 2: {}", "???")
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_1_is_correct() {
+
+    }
+
+    #[test]
+    fn part_2_is_correct() {
+
+    }
+
+    const SAMPLE_INPUT: &str = "\
+[1518-11-01 00:00] Guard #10 begins shift
+[1518-11-01 00:05] falls asleep
+[1518-11-01 00:25] wakes up
+[1518-11-01 00:30] falls asleep
+[1518-11-01 00:55] wakes up
+[1518-11-01 23:58] Guard #99 begins shift
+[1518-11-02 00:40] falls asleep
+[1518-11-02 00:50] wakes up
+[1518-11-03 00:05] Guard #10 begins shift
+[1518-11-03 00:24] falls asleep
+[1518-11-03 00:29] wakes up
+[1518-11-04 00:02] Guard #99 begins shift
+[1518-11-04 00:36] falls asleep
+[1518-11-04 00:46] wakes up
+[1518-11-05 00:03] Guard #99 begins shift
+[1518-11-05 00:45] falls asleep
+[1518-11-05 00:55] wakes up\
+";
+}
