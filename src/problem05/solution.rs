e448b079469120a94fd8fee8348c30e0ee5df3a7
Joel Pedraza <jpedraza@chewy.com>
2018-12-05 22:06:10 -0500

    optim prob 5, add tests and benches

--- src/problem05/solution.rs
+++ src/problem05/solution.rs
@@ -8,38 +8,36 @@ pub fn part2() {
     println!("Problem 5, Part 2: {}", length_of_best_polymer(POLYMER))
 }
 
-fn react(polymer: String) -> String {
-    let mut polymer = polymer.into_bytes();
-    let mut left = 0;
-
-    while left + 1 < polymer.len() {
-        if polymer.has_reaction(left, left+1) {
-            polymer.remove(left);
-            polymer.remove(left);
-            left = left.saturating_sub(1);
-        } else {
-            left += 1;
+fn react(polymer: impl Iterator<Item = u8>) -> String {
+    let mut stack: Vec<u8> = Vec::new();
+    for c in polymer {
+        match stack.last() {
+            None => stack.push(c),
+            Some(&d) => if has_reaction(c, d) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
         }
     }
-
-    String::from_utf8(polymer).expect("output polymer was not valid utf8")
+    String::from_utf8(stack).expect("output polymer was not valid utf8")
 }
 
 fn length_of_polymer_after_reaction(polymer: &str) -> usize {
-    react(polymer.to_owned()).len()
+    react(polymer.bytes()).len()
 }
 
 const ALPHA_LOWER: &str = "abcdefghijklmnopqrstuvwzyz";
 const ALPHA_UPPER: &str = "ABCDEFGHIJKLMNOPQRSTUBWZYZ";
 
 fn length_of_best_polymer(polymer: &str) -> usize {
-    let initial = react(polymer.to_owned());
+    let initial = react(polymer.bytes());
 
-    ALPHA_LOWER.chars()
-        .zip(ALPHA_UPPER.chars())
+    ALPHA_LOWER.bytes()
+        .zip(ALPHA_UPPER.bytes())
         .map(|(lower, upper)| {
-            let mut cand = initial.clone();
-            cand.retain(|c| { c != lower && c != upper });
+            let cand = initial.bytes()
+                .filter(|&c| { c != lower && c != upper });
             react(cand).len()
         })
         .min()
@@ -59,36 +57,51 @@ fn diff_abs(a: u8, b: u8) -> u8 {
 
 const REACTION_DIFFERENCE: u8 = 'a' as u8 - 'A' as u8;
 
-trait  HasReaction {
-    fn has_reaction(&self, left: usize, right: usize) -> bool;
-}
-
-impl HasReaction for [u8] {
-    fn has_reaction(&self, left: usize, right: usize) -> bool {
-        diff_abs(self[left] as u8, self[right] as u8) == REACTION_DIFFERENCE
-    }
-}
-
-impl HasReaction for str {
-    fn has_reaction(&self, left: usize, right: usize) -> bool {
-        self.as_bytes().has_reaction(left, right)
-    }
+fn has_reaction(left: u8, right: u8) -> bool {
+    diff_abs(left, right) == REACTION_DIFFERENCE
 }
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use super::super::input::POLYMER;
 
     const SAMPLE_INPUT: &str = "dabAcCaCBAcCcaDA";
 
     #[test]
-    fn part_1_is_correct() {
+    fn part_1_sample_is_correct() {
         assert_eq!(10, length_of_polymer_after_reaction(SAMPLE_INPUT))
     }
 
     #[test]
-    fn part_2_is_correct() {
+    fn part_2_sample_is_correct() {
         assert_eq!(4, length_of_best_polymer(SAMPLE_INPUT))
     }
+
+    #[test]
+    fn part_1_is_correct() {
+        assert_eq!(11242, length_of_polymer_after_reaction(POLYMER))
+    }
+
+    #[test]
+    fn part_2_is_correct() {
+        assert_eq!(5492, length_of_best_polymer(POLYMER))
+    }
+}
+
+
+#[cfg(all(feature = "unstable", test))]
+mod bench {
+    extern crate test;
+
+    use super::*;
+    use super::super::input::POLYMER;
+    use self::test::Bencher;
+
+    #[bench]
+    fn react_bench(b: &mut Bencher) {
+        b.iter(|| react(POLYMER.bytes()));
+    }
+
 }
